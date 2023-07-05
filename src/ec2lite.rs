fa6dcf54b0484ff059720a4e44095a1f4d4c7e9a
misaim <=>
2023-07-05 12:16:41 +1000

    cargo fmt

--- src/ec2lite.rs
+++ src/ec2lite.rs
@@ -1,19 +1,12 @@
 use std::fmt;
 use std::fs;
 
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
+use aws_config::meta::region::RegionProviderChain;
 use aws_config::SdkConfig;
-use aws_config::{
-    meta::region::RegionProviderChain, 
-};
 
-use aws_sdk_ec2::{
-    config::Region, 
-    //meta::PKG_VERSION, 
-    Client, 
-    //Error
-};
+use aws_sdk_ec2::{config::Region, Client};
 
 #[derive(Serialize, Deserialize)]
 pub struct EC2Instance {
@@ -24,7 +17,13 @@ pub struct EC2Instance {
 
 impl fmt::Display for EC2Instance {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?} -> {} {}", self.get_name(), self.id, self.get_state())
+        write!(
+            f,
+            "{:?} -> {} {}",
+            self.get_name(),
+            self.id,
+            self.get_state()
+        )
     }
 }
 
@@ -54,7 +53,6 @@ impl EC2Instance {
             None => format!("(State Unknown)"),
         }
     }
-
 }
 
 #[derive(Serialize, Deserialize)]
@@ -68,10 +66,7 @@ impl fmt::Display for EC2Instances {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mut output = String::new();
         for instance in self.instances.iter() {
-            output.push_str(format!(
-                "{}\n",
-                instance,
-            ).as_str());
+            output.push_str(format!("{}\n", instance,).as_str());
         }
         write!(f, "Instance List: \n{}", output)
     }
@@ -83,35 +78,35 @@ impl EC2Instances {
     pub fn is_empty(&self) -> bool {
         self.instances.is_empty()
     }
-    pub fn get_region_tag (&self) -> String {
+    pub fn get_region_tag(&self) -> String {
         match &self.region {
             Some(region) => format!(" --region {}", region),
             None => String::new(),
         }
     }
-    pub fn get_profile_tag (&self) -> String {
+    pub fn get_profile_tag(&self) -> String {
         match &self.profile {
             Some(profile) => format!(" --profile {}", profile),
             None => String::new(),
         }
-    } 
-    fn get_instance_id_vec (&self) -> Option<Vec<String>> {
+    }
+    fn get_instance_id_vec(&self) -> Option<Vec<String>> {
         let mut instances: Vec<String> = vec![];
         for instance in self.instances.iter() {
             instances.push(instance.id.to_string())
         }
-         return Some(instances)
+        return Some(instances);
     }
-    pub fn update_metadata_id (&mut self, id: &str, name: Option<&str>, state: Option<&str>) {
+    pub fn update_metadata_id(&mut self, id: &str, name: Option<&str>, state: Option<&str>) {
         for instance in self.instances.iter_mut() {
             if instance.id == id {
                 match name {
                     Some(new_name) => instance.set_name(new_name),
-                    None => ()
+                    None => (),
                 }
                 match state {
                     Some(new_state) => instance.set_state(new_state),
-                    None => ()
+                    None => (),
                 }
             }
         }
@@ -123,55 +118,64 @@ impl EC2Instances {
         self.instances.iter()
     }
     pub fn new() -> Self {
-        Self { 
+        Self {
             instances: vec![],
-            region: None, 
+            region: None,
             profile: None,
         }
     }
-    // Unsafe. Expects to be able to serialise and write an EC2Instances object to json_file. 
+    // Unsafe. Expects to be able to serialise and write an EC2Instances object to json_file.
     pub fn write_json(&self, json_file: &str) {
         let serialized = serde_json::to_string(&self).unwrap();
         fs::write(json_file, &serialized).expect("Unable to write file");
     }
-    // Unsafe. Expects to be able to read from json_file and deserialise to an EC2Instances object. 
+    // Unsafe. Expects to be able to read from json_file and deserialise to an EC2Instances object.
     pub fn read_json(json_file: &str) -> Self {
         let data = fs::read_to_string(json_file).expect("Unable to read file");
         let instances: EC2Instances = serde_json::from_str(&data).unwrap();
-        return instances
+        return instances;
     }
     // Unsafe. Expects SOME sort of AWS config file and valid region. While attempts are made to catch some of these, this is not an AWS credential helper tool.
-    pub async fn new_from_region(region: Option<String>, profile:Option<String>, metadata: EC2GetMetadata) -> Self {
+    pub async fn new_from_region(
+        region: Option<String>,
+        profile: Option<String>,
+        metadata: EC2GetMetadata,
+    ) -> Self {
         let mut return_instances = self::EC2Instances::new();
-        
+
         return_instances.profile = profile.clone();
         return_instances.region = region.clone();
 
         let region_chain = RegionProviderChain::first_try(region.map(Region::new))
             .or_default_provider()
             .or_else(Region::new("us-east-1"));
-        
+
         let config: SdkConfig = match &return_instances.profile {
-            Some(profile_string) => aws_config::from_env().profile_name(profile_string).load().await,
+            Some(profile_string) => {
+                aws_config::from_env()
+                    .profile_name(profile_string)
+                    .load()
+                    .await
+            }
             None => aws_config::from_env().region(region_chain).load().await,
         };
-    
+
         return_instances.region = match config.region() {
-            Some(read_region) => Some(format!("{}",read_region)),
-            None => None
+            Some(read_region) => Some(format!("{}", read_region)),
+            None => None,
         };
-        
+
         let client = Client::new(&config);
 
         // first fill our Ec2instances object with all instances. Currently doesn't read status (here)... funny world we're in.
         let resp = client.describe_instance_status().send().await;
 
         for instance_status in resp.unwrap().instance_statuses().unwrap_or_default() {
-            return_instances.add_instance( EC2Instance::new(
-                instance_status.instance_id().unwrap_or_default()
+            return_instances.add_instance(EC2Instance::new(
+                instance_status.instance_id().unwrap_or_default(),
             ));
         }
-        // ..then keep using the same client to grab metadata, if required. Yes this is somewhat horrible. 
+        // ..then keep using the same client to grab metadata, if required. Yes this is somewhat horrible.
         match metadata {
             EC2GetMetadata(true) => {
                 let instance_list = return_instances.get_instance_id_vec();
@@ -184,35 +188,36 @@ impl EC2Instances {
                                 .send()
                                 .await
                                 .unwrap();
-        
+
                             for reservation in resp.reservations().unwrap_or_default() {
                                 for instance in reservation.instances().unwrap_or_default() {
                                     let instance_id = instance.instance_id().unwrap();
                                     let mut name: Option<&str> = None;
                                     for tag in instance.tags().unwrap() {
                                         match tag.key().unwrap_or_default() {
-                                            "Name" =>  { name = Some(tag.value().unwrap_or_default()) },
-                                            _ => ()
+                                            "Name" => name = Some(tag.value().unwrap_or_default()),
+                                            _ => (),
                                         }
-                                    };
+                                    }
                                     // I was fucking around reading IAM profile names but don't use them :)
                                     //let profile = match instance.iam_instance_profile() {
                                     //    Some(profile_new) => Some(profile_new.arn().unwrap()),
                                     //    None => None
                                     //};
-                                    let state: Option<&str> = match instance.state().unwrap().name() {
+                                    let state: Option<&str> = match instance.state().unwrap().name()
+                                    {
                                         Some(state_new) => Some(state_new.as_str()),
-                                        None => None
+                                        None => None,
                                     };
                                     return_instances.update_metadata_id(instance_id, name, state);
                                 }
                             }
                         }
-                    },
-                    None => ()
+                    }
+                    None => (),
                 }
-            },
-            EC2GetMetadata(false) => ()
+            }
+            EC2GetMetadata(false) => (),
         }
         return return_instances;
     }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
-
 use clap::Parser;
 
 mod ec2lite;
-use crate::ec2lite::EC2Instances;
 use crate::ec2lite::EC2GetMetadata;
+use crate::ec2lite::EC2Instances;
 
-use cursive::Cursive;
 use cursive::traits::*;
-use cursive::views::{
-    Dialog, 
-    Panel,
-    SelectView, 
-};
+use cursive::views::{Dialog, Panel, SelectView};
+use cursive::Cursive;
 
 use arboard::Clipboard;
 
@@ -32,7 +27,11 @@ struct Opt {
 
 #[tokio::main]
 async fn main() -> Result<(), ()> {
-    let Opt { region, profile, _verbose ,} = Opt::parse();
+    let Opt {
+        region,
+        profile,
+        _verbose,
+    } = Opt::parse();
 
     let instances = EC2Instances::new_from_region(region, profile, EC2GetMetadata(true)).await;
 
@@ -40,43 +39,44 @@ async fn main() -> Result<(), ()> {
     //let instances = ec2lite::EC2Instances::read_json("example.json");
     // Write to a json cache file
     //instances.write_json("example.json");
-     
+
     let mut siv = cursive::default();
-    
+
     siv.load_toml(include_str!("../style.toml")).unwrap();
 
     siv.add_global_callback('q', |s| s.quit());
 
     if instances.is_empty() {
-        let no_instances_error_string = format!("No Instances were returned.\nCheck your region and AWS Credentials settings.");
-        siv.add_layer(Dialog::text(no_instances_error_string)
-            .title("Error")
-            .button("Close", |s| s.quit() ) //Default!
+        let no_instances_error_string =
+            format!("No Instances were returned.\nCheck your region and AWS Credentials settings.");
+        siv.add_layer(
+            Dialog::text(no_instances_error_string)
+                .title("Error")
+                .button("Close", |s| s.quit()), //Default!
         )
-    }
-    else {
+    } else {
         let mut instance_select = SelectView::new();
         instance_select.set_on_submit(show_result);
         for instance in instances.iter() {
             instance_select.add_item(
-                format!("{}", &instance),  
+                format!("{}", &instance),
                 format!(
-                    "aws ssm start-session --target {}{}{}", 
+                    "aws ssm start-session --target {}{}{}",
                     &instance.id,
                     &instances.get_profile_tag(),
                     &instances.get_region_tag(),
-                )
+                ),
             );
         }
         siv.add_layer(
             Panel::new(instance_select)
                 .title("Select an instance")
-                .full_screen()
+                .full_screen(),
         );
     }
 
-	siv.run();
-    
+    siv.run();
+
     Ok(())
 }
 
@@ -84,10 +84,11 @@ fn show_result(s: &mut Cursive, msg_input: &str) {
     let mut clipboard = Clipboard::new().unwrap();
     clipboard.set_text(msg_input).unwrap();
 
-    s.add_layer(Dialog::text(format!("{}\n Copied to clipboard.", msg_input))
-        .title("Results")
-        .button("Close", |s| s.quit() ) //Default!
-        .button("Back", |s| close_page(s) ) 
+    s.add_layer(
+        Dialog::text(format!("{}\n Copied to clipboard.", msg_input))
+            .title("Results")
+            .button("Close", |s| s.quit()) //Default!
+            .button("Back", |s| close_page(s)),
     )
 }
 
